76cb640562ca26d92fe97ae14f48768fac76b680
Nicola92 <nicola.rizzi.92@gmail.com>
2019-11-23 00:01:26 +0100

    Terminato esercizio

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//PROVA UNO
 // var lista = []
 //
 // var i= 0;
@@ -9,37 +10,80 @@
 // }
 // }
 
+do {
+    var livelloDifficoltà = parseInt(prompt('Seleziona il livello di difficoltà inserendo un numero 0=difficoltà facile, 1=difficoltà medio, 2=difficoltà elevata'))
+} while (livelloDifficoltà<0 || livelloDifficoltà>2 || isNaN(livelloDifficoltà));
 
+console.log('Hai selezionato il livello di difficoltà ' + livelloDifficoltà)
 
-var lista = [];
-var i= 0;
+switch (livelloDifficoltà) {
+  case 0:
+  var max = 100;
+    console.log();
+    break;
+  case 1:
+  var max = 80;
+    console.log();
+    break;
+  case 2:
+  var max = 50;
+    console.log();
+}
+
+console.log("Desidera qualcos'altro ?");
+
+//creo un array 'mine' vuoto
+var mine = [];
+var i = 0;
 while (i < 16) {
-var num=Math.round(Math.random()*100);
-   if (!lista.includes(num)) {
- lista.push(num);
-i++;
+    var minaGenerata = generaNumeroRandom(1,max);
+    if (!mine.includes(minaGenerata)) {
+        mine.push(minaGenerata);
+        i++;
+    }
 }
+
+function generaNumeroRandom(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
-console.log(lista);
-
-
-
+console.log(mine);
 
  var userlist = [];
- var x = 0;
- while (x < 85) {
- var numeroutente = parseInt(prompt('Inserisci un numero da 1 a 100'))
- if (userlist.includes(numeroutente)) {
- console.log('numero già inserito');
- } else if (lista.includes(numeroutente)) {
-          console.log('Hai perso');
-     x == 85;
-      } else {
-          userlist.push(numeroutente);
-           console.log(userlist);
-          x++;
-      }
-  }
+ var x = 1;
+ var tentativiPossibili = max - 16;
+ var bombaCalpestata = false;
+ while (x <= tentativiPossibili && bombaCalpestata == false) {
+     var numeroUtente = parseInt(prompt('Inserisci un numero da 1 a 100'))
+     if (userlist.includes(numeroUtente)) {
+         console.log('numero già inserito');
+     } else if (numeroNonValido(numeroUtente)) {
+         console.log('numero non valido');
+     } else if (isNaN(numeroUtente)) {
+         console.log('non hai inserito un numero');
+     } else if (mine.includes(numeroUtente)) {
+         console.log('Hai perso');
+         x = tentativiPossibili + 1;
+         bombaCalpestata = true;
+// Se la x raggiunge la quota dei tentativiPossibili, invia messaggio di vittoria, pusha l'ultimo numero nella userlist e aumenta di 1 la var x per far interrompere il ciclo
+     } else if (x == tentativiPossibili) {
+         console.log('Hai vinto');
+         userlist.push(numeroUtente);
+         x++;
+     }
+     else {
+         userlist.push(numeroUtente);
+         console.log(userlist);
+         x++;
+     }
+ }
 
  console.log(userlist);
+ console.log('Hai indovinato ' + userlist.length + ' numeri')
+
+   function numeroNonValido(numeroUtente) {
+       if (numeroUtente < 1 || numeroUtente> 100) {
+       return true;
+       } else {
+          return false;
+      }}
